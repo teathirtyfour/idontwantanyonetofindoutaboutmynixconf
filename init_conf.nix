1a8557eb44f94d36560d04616ea0268c582e5a94
_t34 <90844642+teathirtyfour@users.noreply.github.com>
2026-01-12 12:44:45 +0700

    Install debug
    Install nvidia drivers, msedge, prism, flatpak/hub with sober.

---
+++ init_conf.nix
@@ -0,0 +1,264 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.initrd.kernelModules = [ "i915" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+  boot.loader.grub.enable = true;
+  boot.kernelParams = [ "nvidia_drm.modeset=1" ];
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Jakarta";
+
+  hardware.graphics = {
+    enable = true;
+    # enable32bit = true;
+  };
+  
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      sync.enable = false;
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:3:0:0";
+    };
+  };
+  
+  environment.sessionVariables = {
+    # WLR_NO_HARDWARE_CURSOR = "1";
+    NIXOS_OZONE_WL = "1";
+    # KWIN_FORCE_SW_CURSOR = 1;
+  };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+#   i18n.inputMethod = {
+#     enable = true;
+#     type = "fcitx5";
+#     fcitx5.addons = with pkgs; [
+#       fcitx5-gtk             # Required for GTK apps
+#       fcitx5-mozc-ut
+#       fcitx5-bamboo
+#       # fcitx5-chinese-addons
+#     ];
+#   };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = false;
+    # theme="hyprland_kath";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.t34 = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "storage" "power" "audio" "video" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    kdePackages.discover
+    kdePackages.kcalc
+    kdePackages.kcharselect
+    kdePackages.kclock
+    kdePackages.kcolorchooser
+    kdePackages.kolourpaint
+    kdePackages.ksystemlog
+    kdePackages.sddm-kcm
+    kdiff3
+    kdePackages.isoimagewriter
+    kdePackages.partitionmanager
+    wayland-utils
+    # vencord
+    # (discord.override { withVencord=true; })
+#     vesktop
+    microsoft-edge
+#     kdePackages.dolphin
+#     kdePackages.sddm
+#     sddm-astronaut
+#     vulkan-tools
+    flatpak
+    prismlauncher
+#     zram-generator
+#     tailscale
+#     tailscale-systray
+#     fish
+#     fishPlugins.done
+#     fishPlugins.fzf-fish
+#     fishPlugins.forgit
+#     fishPlugins.hydro
+#     fzf
+#     fishPlugins.grc
+#     grc
+    git
+#     gamemode
+#     flameshot
+#     grim
+#     htop
+#     btop
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+#   services.zram-generator.enable = true;
+#   services.zram-generator.settings = {
+#     zram0 = {
+#       zram-size = 16384;
+#       compression-algorithm = "lzo-rle zstd(level=3) (type=idle)";
+#     };
+#   };
+
+#   services.tailscale.enable = true;
+
+#   programs.bash = {
+#     interactiveShellInit = ''
+#     if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+#     then
+#       shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+#       exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+#     fi
+#     '';
+#   };
+
+#   programs.gamemode.enable = true;
+
+{
+  services.flatpak = {
+    enable = true;
+    # List of Flatpak packages to install or update
+    packages = [
+      "org.vinegarhq.Sober"
+    ];
+    # Optional: Automatically remove packages not listed here
+    removeUnmanagedPackages = false;
+    # Optional: Configure automatic updates
+    update = {
+      auto = {
+        enable = false;
+        onCalendar = "weekly";
+      };
+      duringBuild = false;
+    };
+    # Optional: Configure the default remote
+    remote = {
+      name = "flathub";
+      url = "https://flathub.org/repo/flathub.flatpakrepo";
+    };
+  };
+}
+
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
